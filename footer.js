556371ee2bc68cc20c499738fb3c0d1be5d4338f
Srinath <gmsrinath@gmail.com>
2022-05-07 14:24:50 +0530

    Srinath Pull Request4

--- footer.js
+++ footer.js
@@ -2,5 +2,6 @@ document.write(`
 <footer>
 <p>Author: Open source contributors<br>
 <a href="mailto:test@example.com">test@example.com</a></p>
+<p> Srinath - 2021mt93098 - Open Source Contributor for Assignment 4</p>
 </footer>
 `);
